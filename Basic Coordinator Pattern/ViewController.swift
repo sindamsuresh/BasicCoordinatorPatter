3414e77ed54126e8e4d38e0d62681f6c5b2ac5c6
SureshSindam <sindam.suresh@gmail.com>
2024-01-15 17:13:03 -0600

    Initial Commit

---
+++ Basic Coordinator Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic Coordinator Pattern
+//
+//  Created by Suresh Sindam on 1/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
